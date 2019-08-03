8ceb54ab9dc3a53a0f86a0f93891665b08ab0467
Brandon Startz <brandon.startz@gmail.com>
2019-08-03 20:30:47 +0300

    Finished navmesh, pathfinding, patrolling, seeking player, player movement

--- Scripts/Behavior_Patrol.cs
+++ Scripts/Behavior_Patrol.cs
@@ -5,37 +5,40 @@ using System.Collections;
 public class Behavior_Patrol : MonoBehaviour
 {
     public Transform[] points;
+
     private int destPoint = 0;
     private NavMeshAgent agent;
     
     void Start () {
         agent = GetComponent<NavMeshAgent>();
 
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
-        agent.autoBraking = false;
+        agent.autoBraking = false; // Disabling auto-braking allows for continuous movement between points (ie, the agent doesn't slow down as it pproaches a destination point).
+    }
 
+    public void ManualStart(){ 
+        //Debug.Log("Starting patrol");
         GotoNextPoint();
     }
 
+    //Turns out player-seeking did not need to manually stop patrol
+    // public void ManualStop(){
+    //     Debug.Log("Trying to stop patrol, brace for error");
+    //     isPatrolling = false;
+    // }
+
     void GotoNextPoint() {
-        // Returns if no points have been set up
-        if (points.Length == 0)
-            return;
+        if(points.Length > 0){
+            // Set the agent to go to the currently selected destination.
+            agent.destination = points[destPoint].position;
 
-        // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
-
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+            // Choose the next point in the array as the destination, cycling to the start if necessary.
+            destPoint = (destPoint + 1) % points.Length;
+        }
     }
 
     void Update () {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        // Choose the next destination point when the agent gets close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.3f)
             GotoNextPoint();
     }
 }
---
+++ Scripts/EnemyBehaviorController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+//This should be the manager for all enemy behavior
+//Behavior_patrol should be the default behavior until they get in range, then they attack (player in range script takes over)
+public class EnemyBehaviorController : MonoBehaviour
+{
+    public float movementSpeed = 10f; //This should be changed because currently the player can outrun the guards
+    
+    float searchRadius = 2.5f; //How far will the guards look for the player
+    GameObject player;
+    bool playerFound;
+    Vector3 target;
+    Behavior_Patrol patrolBehavior;
+
+    void Start()
+    {
+        patrolBehavior = gameObject.GetComponent<Behavior_Patrol>(); //Grab Behavior control on game object which includes nav points for that enemy
+        patrolBehavior.ManualStart();
+        //Debug.Log("Attempting to manually start patrol" + patrolBehavior);
+
+        OnDrawGizmos();
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+    }
+
+    void Update()
+    {
+        ScanForPlayer();
+        
+        if(!playerFound){
+            //Check for proximity to player, and if within range, call PlayerInRange and cutoff Behavior patrol
+            ScanForPlayer();
+        }
+        else{
+            SeekAndDestroy(player);
+        }  
+    }
+
+    void ScanForPlayer(){
+        player = GameObject.FindGameObjectWithTag("Player");
+        //Debug.Log("hopefully found player " + player);
+
+        if(player != null){ //Found the player
+            if ((Vector3.Distance(player.transform.position, transform.position)) <= searchRadius)
+            {
+                //Got him in range, attack
+                //Debug.Log("Player in range");
+                playerFound = true;
+            }
+        }
+    }
+
+    void SeekAndDestroy(GameObject target){
+        // Move our position a step closer to the target.
+        float step = movementSpeed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(this.transform.position, target.transform.position) < 1f)
+        {
+            KillPlayer();
+        }
+    }
+
+    void KillPlayer(){
+        Destroy(player);
+        patrolBehavior.ManualStart();
+        playerFound = false;
+        target = Vector3.zero;
+    }
+}
+
+
