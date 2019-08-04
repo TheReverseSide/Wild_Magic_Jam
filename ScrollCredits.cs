c79f2f382522f24dd90f1fd7036597438bac3f5e
Eliza Grafton <eliza.m.grafton@gmail.com>
2019-08-04 13:33:30 -0400

    guard spinning fixed

---
+++ ScrollCredits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCredits : MonoBehaviour
+{
+    public float startHeight;
+    public float endHeight;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.up * (endHeight - startHeight) / time * Time.deltaTime;
+
+        if (transform.localPosition.y >= endHeight)
+        {
+            SceneSwapper.swapper.GoToNextLevel();
+        }
+    }
+}
