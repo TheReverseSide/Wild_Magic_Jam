16a381460eb26b9ded65af7bb449cb4dfb7be1c6
Brandon Startz <brandon.startz@gmail.com>
2019-08-04 16:31:00 +0300

    Added more sprites, more animation tweaking, AI changes

---
+++ Scripts/AI and Player Movement/Guard_Anim_Flipper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard_Anim_Flipper : MonoBehaviour
+{
+    bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if ((horizontalInput > 0 || verticalInput > 0) && !facingRight)
+    //     {
+    //         Flip();
+    //     }
+    //     else if ((horizontalInput < 0 || verticalInput < 0) && facingRight)
+    //     {
+    //         Flip();
+    //     }
+    // }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
