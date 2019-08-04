03dbf29b0de726481a427e0cd7a1a656423e3448
Eliza Grafton <eliza.m.grafton@gmail.com>
2019-08-04 12:24:01 -0400

    walls

---
+++ NavMeshConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavMeshConverter : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.GetComponent<NavMeshObstacle>().center = gameObject.GetComponent<BoxCollider>().center;
+        gameObject.GetComponent<NavMeshObstacle>().size = gameObject.GetComponent<BoxCollider>().size;
+    }
+}
